64027c261e5582c5a860ea2bc0edce583650650c
aldm <d44.a1@yandex.ru>
2024-05-26 17:56:57 +0300

    1

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@ videoPreview.addEventListener("click", function () {
   videoPreview.style.display = "none";
   videoPlayer.play();
 });
+
+const inputField = document.getElementById("input-field");
+
+inputField.addEventListener("focus", function () {
+  inputField.classList.add("input-focus");
+});
+
+//инпут плейсхолдер при фокусе
+const input = document.getElementById("input");
+const label = input.nextElementSibling;
+
+input.addEventListener("focus", function () {
+  label.classList.add("active");
+});
+
+input.addEventListener("blur", function () {
+  if (input.value === "") {
+    label.classList.remove("active");
+  }
+});
+
+if (input.value !== "") {
+  label.classList.add("active");
+}

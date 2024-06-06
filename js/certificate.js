a7d3a5029e1ba97876c5b724373ac4d02dab7f56
aldm <d44.a1@yandex.ru>
2024-06-06 21:10:12 +0300

    1

--- js/certificate.js
+++ js/certificate.js
@@ -1,27 +1,9 @@
-// const containerElement = document.querySelector(".certificate-page__items");
-// const buttonElement = document.querySelector(".certificate-page__button");
-// const contElemHidden = document.querySelector(
-//   ".certificate-page__items_hidden"
-// );
-
-// if (containerElement.children.length < 12) {
-//   buttonElement.style.display = "none";
-// } else {
-//   buttonElement.style.display = "block";
-// }
-
-// buttonElement.addEventListener("click", () => {
-//   contElemHidden.style.display = "flex";
-//   buttonElement.style.display = "none";
-// });
-// Получаем элементы для первого блока
 const containerElement = document.querySelector(".certificate-page__items");
 const buttonElement = document.querySelector(".certificate-page__button");
 const contElemHidden = document.querySelector(
   ".certificate-page__items_hidden"
 );
 
-// Проверяем количество элементов в первом блоке
 if (containerElement) {
   if (containerElement.children.length < 12) {
     buttonElement.style.display = "none";
@@ -30,13 +12,11 @@ if (containerElement) {
   }
 }
 
-// Добавляем обработчик клика на кнопку первого блока
 buttonElement.addEventListener("click", () => {
   contElemHidden.style.display = "flex";
   buttonElement.style.display = "none";
 });
 
-// Получаем элементы для второго блока
 const projectsContainerElement = document.querySelector(".projects__items");
 const projectsButtonElement = document.querySelector(
   ".certificate-page__button_cases"
@@ -45,7 +25,6 @@ const projectsContElemHidden = document.querySelector(
   ".projects__items_hidden"
 );
 
-// Проверяем количество элементов во втором блоке
 if (projectsContainerElement) {
   if (projectsContainerElement.children.length < 12) {
     projectsButtonElement.style.display = "none";
@@ -54,7 +33,6 @@ if (projectsContainerElement) {
   }
 }
 
-// Добавляем обработчик клика на кнопку второго блока
 projectsButtonElement.addEventListener("click", () => {
   projectsContElemHidden.style.display = "flex";
   projectsButtonElement.style.display = "none";

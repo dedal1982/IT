077c613331c68129bc03ebd2f01838f6719e81f2
aldm <d44.a1@yandex.ru>
2024-06-06 21:09:31 +0300

    1

--- js/certificate.js
+++ js/certificate.js
@@ -1,16 +1,61 @@
+// const containerElement = document.querySelector(".certificate-page__items");
+// const buttonElement = document.querySelector(".certificate-page__button");
+// const contElemHidden = document.querySelector(
+//   ".certificate-page__items_hidden"
+// );
+
+// if (containerElement.children.length < 12) {
+//   buttonElement.style.display = "none";
+// } else {
+//   buttonElement.style.display = "block";
+// }
+
+// buttonElement.addEventListener("click", () => {
+//   contElemHidden.style.display = "flex";
+//   buttonElement.style.display = "none";
+// });
+// Получаем элементы для первого блока
 const containerElement = document.querySelector(".certificate-page__items");
 const buttonElement = document.querySelector(".certificate-page__button");
 const contElemHidden = document.querySelector(
   ".certificate-page__items_hidden"
 );
 
-if (containerElement.children.length < 12) {
-  buttonElement.style.display = "none";
-} else {
-  buttonElement.style.display = "block";
+// Проверяем количество элементов в первом блоке
+if (containerElement) {
+  if (containerElement.children.length < 12) {
+    buttonElement.style.display = "none";
+  } else {
+    buttonElement.style.display = "block";
+  }
 }
 
+// Добавляем обработчик клика на кнопку первого блока
 buttonElement.addEventListener("click", () => {
   contElemHidden.style.display = "flex";
   buttonElement.style.display = "none";
+});
+
+// Получаем элементы для второго блока
+const projectsContainerElement = document.querySelector(".projects__items");
+const projectsButtonElement = document.querySelector(
+  ".certificate-page__button_cases"
+);
+const projectsContElemHidden = document.querySelector(
+  ".projects__items_hidden"
+);
+
+// Проверяем количество элементов во втором блоке
+if (projectsContainerElement) {
+  if (projectsContainerElement.children.length < 12) {
+    projectsButtonElement.style.display = "none";
+  } else {
+    projectsButtonElement.style.display = "block";
+  }
+}
+
+// Добавляем обработчик клика на кнопку второго блока
+projectsButtonElement.addEventListener("click", () => {
+  projectsContElemHidden.style.display = "flex";
+  projectsButtonElement.style.display = "none";
 });

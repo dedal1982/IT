480b80372898a2d29f41f2c3cd1f4a8552eeca2a
aldm <d44.a1@yandex.ru>
2024-06-21 22:59:02 +0300

    1

--- js/index.js
+++ js/index.js
@@ -169,28 +169,6 @@ inputField.forEach((input) => {
     label.classList.add("active");
   });
 });
-// const inputContainers = document.querySelectorAll(
-//   ".form-group,.input,.form-group-textarea"
-// );
-// const inputField = document.querySelectorAll(".input,.textarea");
-
-// if (inputContainers) {
-//   inputContainers.forEach((container) => {
-//     container.addEventListener("click", () => {
-//       const label = container.querySelector(".label");
-//       if (label) {
-//         label.classList.add("active");
-//       }
-//     });
-//   });
-// }
-
-// inputField.forEach((input) => {
-//   input.addEventListener("focus", () => {
-//     const label = input.parentElement.querySelector(".label");
-//     label.classList.add("active");
-//   });
-// });
 
 // открытие - закрытие формы десктоп
 const popupOverlay = document.querySelector(".popup-overlay");
